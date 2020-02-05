cb8d4f1e7fa963bc6c6896d2bae8206ca4cce3d9
Oliver <oliversouthon@gmail.com>
2020-02-05 14:13:28 +0800

    second-commit-base-functionality

--- start.py
+++ start.py
@@ -1,10 +1,18 @@
 import os
 import PySimpleGUI as sg 
 
-path = input("Enter path of foler: ")
-new_name = input("Rewrite the original file path with the new file name")
+folder = input("Enter path of folder: ")
+file_type = input("Enter type of files in that folder (.jpg, .png, etc): ")
 
-def RenameImage(path, new_name):
-    os.rename(path, new_name)
+def main(folder, file_type):
 
-RenameImage(path, new_name)
+    i=1
+    for filename in os.listdir(folder):
+        src = folder + "\\" + filename
+        new_src = folder + "\\" + str(i) + file_type
+        os.rename(src, new_src)
+        i = int(i)
+        i += 1
+
+
+main(folder, file_type)
